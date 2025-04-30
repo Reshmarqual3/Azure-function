de44f7a17236130b033607a6f43a1012f2692f8a
Reshmarqual3 <133945743+Reshmarqual3@users.noreply.github.com>
2025-04-30 20:01:10 +0530

    Update res.py

--- res.py
+++ res.py
@@ -3,4 +3,4 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse("Hello from Azure Function!") 
+    return func.HttpResponse("Hello from Azure Function Reshma!")
